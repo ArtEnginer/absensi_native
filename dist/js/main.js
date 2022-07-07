9bb7c595c5dbb1d4ef6d42dafec3ad1cfb77a734
MrFrost-Nv27 <nazruelyourgoodfriend@gmail.com>
2022-07-07 10:50:35 +0700

    Add Kop

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,37 @@
-$(document).ready(function () {
+const getBase64FromUrl = async (url) => {
+  const data = await fetch(url);
+  const blob = await data.blob();
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(blob);
+    reader.onloadend = () => {
+      const base64data = reader.result;
+      resolve(base64data);
+    };
+  });
+};
+
+$(document).ready(async function () {
+  const koppp = await getBase64FromUrl("dist/img/kopsurat.png");
+
   $(".datatables-init").DataTable({
     responsive: true,
     dom: "Bfrtip",
-    buttons: ["copyHtml5", "excelHtml5", "csvHtml5", "pdfHtml5"],
+    buttons: [
+      "copyHtml5",
+      "excelHtml5",
+      "csvHtml5",
+      {
+        extend: "pdfHtml5",
+        customize: function (doc) {
+          doc.content.splice(1, 0, {
+            margin: [0, 0, 0, 12],
+            alignment: "center",
+            image: koppp,
+            width: 560,
+          });
+        },
+      },
+    ],
   });
 });
